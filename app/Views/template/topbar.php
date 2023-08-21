6c60e5a02c632fab4ddde2e4e7f30dc10af3cac6
fatah1707 <fatahahmad807@gmail.com>
2023-08-21 10:07:38 +0700

    form post login

--- app/Views/template/topbar.php
+++ app/Views/template/topbar.php
@@ -24,13 +24,15 @@
         <li class="nav-item"><a class="nav-link" href="#">Pusat Bantuan</a></li>
     </ul>
     
-    <button type="button" class="btn btn-secondary" id="registerButton">Registrasi</button>
+    <button type="button" class="btn btn-secondary" id="registerButton">Register</button>
     <button type="button" class="btn btn-primary" id="loginButton">Login</button>
 </div>
 
 
     </div>
 </nav>
+
+<!--ini adalah modal untuk register dan login -->
 <script>
         document.getElementById('registerButton').addEventListener('click', function() {
             Swal.fire({
@@ -68,38 +70,54 @@
     </script>
 
 <script>
-        document.getElementById('loginButton').addEventListener('click', function() {
-            Swal.fire({
-                title: 'Login',
-                html:
-                    '<input type="text" id="username" class="swal2-input" placeholder="Username">' +
-                    '<div class="password-container">' +
-                    '<input type="password" id="password" class="swal2-input password-input" placeholder="Password">' +
-                    '<br>' +
-                    '<input type="checkbox" id="showPassword" class="show-password"> <label for="showPassword">Lihat Password</label>' +
-                    '</div>',
-                focusConfirm: false,
-                didOpen: () => {
-                    const passwordInput = Swal.getPopup().querySelector('.password-input');
-                    const showPasswordCheckbox = Swal.getPopup().querySelector('.show-password');
+    document.getElementById('loginButton').addEventListener('click', function() {
+        Swal.fire({
+            title: 'Login',
+            html:
+                '<form id="loginForm" action="/login" method="POST">' +
+                '<input type="text" id="username" name="username" class="swal2-input" placeholder="Username">' +
+                '<div class="password-container">' +
+                '<input type="password" id="password" name="password" class="swal2-input password-input" placeholder="Password">' +
+                '<br>' +
+                '<input type="checkbox" id="showPassword" class="show-password"> <label for="showPassword">Lihat Password</label>' +
+                '</div>' +
+                '</form>',
+            focusConfirm: false,
+            didOpen: () => {
+                const passwordInput = Swal.getPopup().querySelector('.password-input');
+                const showPasswordCheckbox = Swal.getPopup().querySelector('.show-password');
 
-                    showPasswordCheckbox.addEventListener('change', () => {
-                        passwordInput.type = showPasswordCheckbox.checked ? 'text' : 'password';
-                    });
-                },
-                preConfirm: () => {
-                    const username = Swal.getPopup().querySelector('#username').value;
-                    const passwordInput = Swal.getPopup().querySelector('#password');
-                    const password = passwordInput.value;
+                showPasswordCheckbox.addEventListener('change', () => {
+                    passwordInput.type = showPasswordCheckbox.checked ? 'text' : 'password';
+                });
+            },
+            preConfirm: () => {
+                const form = Swal.getPopup().querySelector('#loginForm');
+                const formData = new FormData(form);
 
-                    // Di sini Anda dapat menambahkan logika untuk memeriksa kredensial
-                    // Contoh: if (username === 'admin' && password === 'password') { ... }
-                    // Jika berhasil, tampilkan pesan sukses, jika gagal, tampilkan pesan error
-                    Swal.fire('Logged In', 'Anda telah login', 'success');
-                }
-            });
+                // Kirim data menggunakan fetch atau XMLHttpRequest
+                fetch('/login', {
+                    method: 'POST',
+                    body: formData
+                })
+                .then(response => response.json())
+                .then(data => {
+                    // Menggunakan respons dari server untuk menampilkan pesan
+                    if (data.success) {
+                        Swal.fire('Logged In', 'Anda telah login', 'success');
+                    } else {
+                        Swal.fire('Login Failed', 'Username atau password salah', 'error');
+                    }
+                })
+                .catch(error => {
+                    console.error('Error:', error);
+                    Swal.fire('Error', 'Terjadi kesalahan saat melakukan login', 'error');
+                });
+            }
         });
-    </script>
+    });
+</script>
+
 
 
 
